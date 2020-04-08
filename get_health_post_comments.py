5ba66d487ae0ee186006365acd8f8114ff55fce5
James Stubbs <stubbs@sfu.ca>
2020-04-08 09:27:22 -0700

    Initial commit

---
+++ get_health_post_comments.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import praw
+
+my_client_id = "yixcjhi4bhbC_w"
+my_client_secret = "lqeYBL1SXuiTPLDc9zoeM1QbvnM"
+my_user_agent = "CMNS801_research_project"
+
+reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret,
+                     user_agent=my_user_agent)
+
+df_rows = []
+
+
+# collect top posts from r/health
+
+# get comments from a specified submission
+def get_comments(submission_id):
+    submission = reddit.submission(id=submission_id)
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        df_rows.append([comment.id, comment.score,
+                        comment.created, comment.body])
+    print(f"Added ~ {len(submission.comments)} comments")
+
+
+if __name__ == '__main__':
+    df_input = pd.read_csv('data/top_health_posts.csv')
+    submission_ids = df_input.id.tolist()
+    for submission_id in submission_ids:
+        get_comments(submission_id)
+    df_output = pd.DataFrame(df_rows, columns=[
+        'comment_id', 'comment_score', 'comment_created', 'comment_body'])
+    df_output.to_csv('data/comments_from_top_health_posts.csv')
---
+++ get_health_posts.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import praw
+import time
+
+my_client_id = "yixcjhi4bhbC_w"
+my_client_secret = "lqeYBL1SXuiTPLDc9zoeM1QbvnM"
+my_user_agent = "CMNS801_research_project"
+
+reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret,
+                     user_agent=my_user_agent)
+
+# collect top posts from r/health
+
+
+def collect_posts():
+    start_time = time.time()
+    posts = []
+    print('Starting scrape...')
+    hot_posts = reddit.subreddit('News').hot(
+        time_filter='month', limit=None)
+    for post in hot_posts:
+        posts.append([post.title, post.score, post.id, post.subreddit, post.url,
+                      post.num_comments, post.selftext, post.created])
+        df = pd.DataFrame(posts, columns=['title', 'score', 'id', 'subreddit',
+                                          'url', 'num_comments', 'body', 'created'])
+        df.to_csv('data/top_health_posts.csv')
+    print(f'Scraped {len(posts)} in {time.time() - start_time}')
+
+
+# execute
+if __name__ == '__main__':
+    collect_posts()
